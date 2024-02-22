659371bce05f0936f8fd363f29d277a0a2cfa528
chibaba <chinedu.onyeoka@gmail.com>
2024-02-23 00:35:11 +0100

    setting up infras

--- infras/gitops3/iteration2/main.tf
+++ infras/gitops3/iteration2/main.tf
@@ -2,3 +2,28 @@ variable "environment" {
   description = "The environment e.g uat or prod or dev"
   type        = string
 }
+
+variable "region" {
+  description = "The region where we wish to deploy to"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+locals {
+  name-suffix = "${var.region}-${var.environment}"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.10"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
