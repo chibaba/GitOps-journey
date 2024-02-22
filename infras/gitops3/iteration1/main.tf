b26370a12034cd163b9ac64dd465a9c46d4844d8
chibaba <chinedu.onyeoka@gmail.com>
2024-02-23 00:20:09 +0100

    wip

---
+++ infras/gitops3/iteration1/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorps/aws"
+        version = "4.10"
+    }
+  }
+  required_version = ">=1.1.0"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "apache2_server" {
+    ami           = "ami-00399ec92321828f5"
+  instance_type = "t2.micro"
+  user_data = "${file("user_data.sh")}"
+
+  tag = {
+    env = "dev"
+  }
+}
