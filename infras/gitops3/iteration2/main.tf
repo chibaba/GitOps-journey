09c97319be8a63c56658292a2a40fe441144531a
chibaba <chinedu.onyeoka@gmail.com>
2024-02-23 17:31:51 +0100

    wip

--- infras/gitops3/iteration2/main.tf
+++ infras/gitops3/iteration2/main.tf
@@ -27,3 +27,64 @@ terraform {
 
   required_version = ">= 1.1.0"
 }
+provider "aws" {
+  region = var.region
+}
+
+# Data element fetching the AMI ID of Ubuntu 20.04
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  # AWS ID of the Canonical organisation
+  owners = ["099720109477"]
+}
+
+# Creating Security Group with ingress/egress rules
+resource "aws_security_group" "public_http_sg" {
+  name = "public_http_sg-${local.name-suffix}"
+
+# Allow all inbound traffic to port 80
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+# Allow EC2 to be connected from internet
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+
+  tags = {
+    "Environment" = var.environment
+    "visibility"  = "public"
+  }
+}
+
+# AWS EC2 Resource creation
+resource "aws_instance" "apache2_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.public_http_sg.id]
+  user_data = "${file("user_data.sh")}"
+  tags = {
+    env  = var.environment
+    Name = "ec2-${local.name-suffix}"
+  }
+}
+
+# Output the Public IP Address
+output "ip_address" {
+  value = aws_instance.apache2_server.public_ip
+}
