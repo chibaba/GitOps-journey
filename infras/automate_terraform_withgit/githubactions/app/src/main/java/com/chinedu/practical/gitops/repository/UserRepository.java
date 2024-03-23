d191245990033d9b8e56d4fcb4268635d3e51a51
chibaba <chinedu.onyeoka@gmail.com>
2024-03-23 16:49:24 +0100

    simple java app

--- infras/automate_terraform_withgit/githubactions/app/src/main/java/com/chinedu/practical/gitops/repository/UserRepository.java
+++ infras/automate_terraform_withgit/githubactions/app/src/main/java/com/chinedu/practical/gitops/repository/UserRepository.java
@@ -1,1 +1,13 @@
 package main.java.com.chinedu.practical.gitops.repository;
+
+package com.chinedu.practical.gitops.repository;
+
+import java.util.List;
+import com.chinedu.practical.gitops.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByName(String name);
+}
